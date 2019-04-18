0c68fd5230c3ac368fed187b3a6999568f8b45e3
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-18 19:51:52 +0000

    Automatically backed up by Learn

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,14 +1,12 @@
 class TipsController < ApplicationController
   def index
-    resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
-      req.params['oauth_token'] = session[:token]
-      req.params['v'] = '20160201'
-    end
+    foursquare = FoursquareService.new.list_tips()
     @results = JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
   end
 
   def create
-    foursquare = FoursquareService.new.create_tip(session[:token], params[:venue_id], params[:tip])
+    foursquare = FoursquareService.new
+    foursquare.create_tip(session[:token], params[:venue_id], params[:tip])
 
     redirect_to tips_path
   end
